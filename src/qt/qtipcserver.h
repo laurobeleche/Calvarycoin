38989c0ba8c82437bda5c6965b69fab616d58ee1
laurobeleche <laurobeleche@gmail.com>
2018-08-14 17:00:43 +0000

    first

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define calvarycoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "calvarycoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
